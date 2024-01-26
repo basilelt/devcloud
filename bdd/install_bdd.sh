7a7aa7560514c7ec859ab47e02fca321b50db7aa
Basile <lethiecb@protonmail.com>
2024-01-26 18:32:33 +0100

    fix

--- bdd/install_bdd.sh
+++ bdd/install_bdd.sh
@@ -13,7 +13,7 @@ After=docker.service
 [Service]
 Type=oneshot
 RemainAfterExit=true
-WorkingDirectory=/home/web/devcloud/bdd
+WorkingDirectory=/home/bdd/devcloud/bdd
 ExecStart=/usr/bin/docker-compose up -d --remove-orphans --build
 ExecStop=/usr/bin/docker-compose down
 
@@ -25,6 +25,6 @@ systemctl enable bdd.service
 systemctl daemon-reload
 
 sudo -u bdd git clone https://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@github.com/basilelt/devcloud
-cd /home/web/devcloud/bdd
+cd /home/bdd/devcloud/bdd
 
 systemctl start bdd.service
